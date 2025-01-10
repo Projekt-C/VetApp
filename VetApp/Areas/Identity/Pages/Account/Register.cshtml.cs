0095b1adb8673d6900b54788c1e620c02005b7a4
PanCamel <wbalucki18@gmail.com>
2025-01-10 21:04:33 +0100

    Registration working

--- VetApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ VetApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -129,9 +129,11 @@ namespace VetApp.Areas.Identity.Pages.Account
                 user.Phone = Input.Phone;
                 user.Email = Input.Email;
                 user.Password = Input.Password;
+                await _userManager.AddToRoleAsync(user, "User");
                 await _userStore.SetUserNameAsync(user, Input.Email, CancellationToken.None);
                 await _emailStore.SetEmailAsync(user, Input.Email, CancellationToken.None);
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                
 
                 if (result.Succeeded)
                 {
